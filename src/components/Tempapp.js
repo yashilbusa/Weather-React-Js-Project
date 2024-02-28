9599db5a85ad6ec0e6ca87b34adddadad7101088
yashilbusa <yashilpatel1914@gmail.com>
2024-02-28 18:27:57 +0530

    c

--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -1,16 +1,39 @@
 import React from "react";
 import "./css/style.css";
 
-const Tempapp = () =>{
-    return(
+const Tempapp = () => {
+    return (
         <>
             <div className="box">
                 <div className="inputdata">
                     <input
-                    type="search"
-                    className="inputfield"/>
+                        type="search"
+                        className="inputfield"
+                        onChange={(event) => {
+
+                        }} />
                 </div>
+
+
+                <div className="info">
+                    <h2 className="location">
+                        <i class="fas fa-street-view"></i>Pune
+                    </h2>
+
+                    <h1 className="temp">
+                        5.25cel
+                    </h1>
+
+                    <h3 className="tempmin_max"> Min : 5.25 celisus | Max : 5.25 celisus</h3>
+
+                </div>
+
+                <div className="wave -one"></div>
+                <div className="wave -two"></div>
+                <div className="wave -three"></div>
+
             </div>
+
         </>
     )
 }

519726b5076bfea41f6389cc9d8849b262aad8da
yashilbusa <yashilpatel1914@gmail.com>
2024-02-28 17:56:08 +0530

    c

---
+++ src/components/Tempapp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./css/style.css";
+
+const Tempapp = () =>{
+    return(
+        <>
+            <div className="box">
+                <div className="inputdata">
+                    <input
+                    type="search"
+                    className="inputfield"/>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Tempapp;
